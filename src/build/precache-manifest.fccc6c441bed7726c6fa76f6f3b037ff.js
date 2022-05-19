72cc446b12d0144b92e11b0ffe39075260a8c977
Mani-Ni <69885302+Mani-Ni@users.noreply.github.com>
2022-05-19 08:54:27 +0530

    fiist commit for react node mongo for hostng process

---
+++ src/build/precache-manifest.fccc6c441bed7726c6fa76f6f3b037ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "260b23759aabf1523217",
+    "url": "/static/css/main.17532e32.chunk.css"
+  },
+  {
+    "revision": "260b23759aabf1523217",
+    "url": "/static/js/main.989f2d2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8ef2300de7f6a072daa",
+    "url": "/static/js/2.77bad49e.chunk.js"
+  },
+  {
+    "revision": "7a076c16cf9419c7addf7c15caf5dd0b",
+    "url": "/index.html"
+  }
+];
